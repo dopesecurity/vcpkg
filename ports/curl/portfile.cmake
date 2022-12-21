28a50843e47c19b416d1b53893d8bd2cdb4666ec
Eoghan Doyle <eoghan@dope.security>
2022-12-21 12:07:36 +0000

    Force poll

--- ports/curl/portfile.cmake
+++ ports/curl/portfile.cmake
@@ -12,7 +12,7 @@ vcpkg_from_github(
         0022-deduplicate-libs.patch
         mbedtls-ws2_32.patch
         export-components.patch
-        eagain.patch
+        force-poll.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
